307330884a40d22cf7d03220dbbcdbe72cfaadf8
Filippo Addabbo <filippoaddabbo1@gmail.com>
2025-07-01 12:12:25 +0200

    feat(about): add About component with HTML, SCSS, and test setup
    - Implemented the About component with a detailed introduction section.
    - Added styling for the About component to enhance layout and visuals.
    - Created a test suite for the About component to ensure proper functionality.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'about', pathMatch: 'full'},
+    {path: 'about', loadComponent: () => import('./about/about.component').then(m => m.AboutComponent)},
+];
