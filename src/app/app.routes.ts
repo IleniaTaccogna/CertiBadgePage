143a511704cda72297558bd7d957e5486043e6d7
Filippo Addabbo <filippoaddabbo1@gmail.com>
2025-07-01 15:59:19 +0200

    style: adjust navbar alignment and improve mobile menu positioning

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    { path: '', loadComponent: () => import('./components/hero/hero.component').then(m => m.HeroComponent) },
     { path: 'home', loadComponent: () => import('./components/hero/hero.component').then(m => m.HeroComponent) },
 ];
