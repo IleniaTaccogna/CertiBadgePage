01ebd6e9707843597f345d59748689114f0c618d
Filippo Addabbo <filippoaddabbo1@gmail.com>
2025-07-02 16:10:45 +0200

    feat: add Piani component with HTML, SCSS, and test files

---
+++ src/app/components/piani/piani.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-piani',
+  imports: [],
+  templateUrl: './piani.component.html',
+  styleUrl: './piani.component.scss'
+})
+export class PianiComponent {
+
+}
