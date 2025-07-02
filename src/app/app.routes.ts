423b9339a57dfb6966d6bea390785aa038ccde18
Ilenia Taccogna <itaccogna@codearchitects.com>
2025-07-02 14:56:44 +0200

    feat: add workflow component with HTML, SCSS, TypeScript, and unit tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./components/hero/hero.component').then(m => m.HeroComponent) },
     { path: 'home', loadComponent: () => import('./components/hero/hero.component').then(m => m.HeroComponent) },
-    {path:'feature', loadComponent: () => import('./components/feature/feature.component').then(c => c.FeatureComponent)}
-
+    {path:'feature', loadComponent: () => import('./components/feature/feature.component').then(c => c.FeatureComponent)},
+    {path:'workflow', loadComponent: () => import('./components/workflow/workflow.component').then(c => c.WorkflowComponent)},
 ];
