7a6cb8bf7c4e6c164b06adcc2af0746d7da17910
Filippo Addabbo <filippoaddabbo1@gmail.com>
2025-07-03 11:27:18 +0200

    fix(routes): restore redirect to home and adjust about route position

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'about', pathMatch: 'full'},
-    {path: 'about', loadComponent: () => import('./components/about/about.component').then(m => m.AboutComponent)},
-
-    { path: '', loadComponent: () => import('./components/hero/hero.component').then(m => m.HeroComponent) },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadComponent: () => import('./components/hero/hero.component').then(m => m.HeroComponent) },
+    { path: 'about', loadComponent: () => import('./components/about/about.component').then(m => m.AboutComponent) },
     { path: 'feature', loadComponent: () => import('./components/feature/feature.component').then(c => c.FeatureComponent) },
     { path: 'workflow', loadComponent: () => import('./components/workflow/workflow.component').then(c => c.WorkflowComponent) },
     { path: 'piani', loadComponent: () => import('./components/piani/piani.component').then(c => c.PianiComponent) }
