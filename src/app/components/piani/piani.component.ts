6da71f7b40c7dd15ef57e085856bf58970766800
Filippo Addabbo <filippoaddabbo1@gmail.com>
2025-07-03 12:45:23 +0200

    feat: add testimonial feature with JSON server integration
    - Added json-server dependency to simulate backend for testimonials.
    - Created testimonial model to define the structure of testimonial data.
    - Implemented TestimonialService to fetch testimonials from the JSON server.
    - Updated PianiComponent to retrieve and display testimonials dynamically.
    - Refactored piani.component.html to use Angular's structural directives for rendering testimonials.
    - Added testimonial.json file to serve as mock data for the testimonials.
    - Created unit tests for TestimonialService to ensure functionality.
    - Minor formatting and code cleanup in AboutComponent.

--- src/app/components/piani/piani.component.ts
+++ src/app/components/piani/piani.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TestimonialService } from '../../services/testimonial.service';
+import { Testimonial } from '../../models/testimonial';
 
 @Component({
   selector: 'app-piani',
@@ -6,6 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './piani.component.html',
   styleUrl: './piani.component.scss'
 })
-export class PianiComponent {
+export class PianiComponent implements OnInit {
+
+  public testimonials: Testimonial[] = [];
+  private _testimonialService = inject(TestimonialService);
+
+  ngOnInit(): void {
+    this._testimonialService.getTestimonials().subscribe(data => {
+      this.testimonials = data
+    })
+  }
+
+
 
 }
